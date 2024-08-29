dc08513f0f931da561c71438d912c45d3d2d4aef
Augusto Riccardi <augustoriccardi@gmail.com>
2024-08-29 15:54:44 -0300

    ondelete:cascade on property author of model Book

--- components/authors.tsx
+++ components/authors.tsx
@@ -81,6 +81,7 @@ export function Sidebar({ selectedAuthors, allAuthors }: SidebarProps) {
         const newParams = new URLSearchParams(
           newAuthors.map((author) => ["author", author])
         );
+
         router.push(`/?${newParams}`);
       });
     },
